86b70214243a82e36e038c30464676432dad00db
weirukai <2624411907@qq.com>
2018-12-14 21:24:34 +0800

    Initial Commit

---
+++ pages/component/index.js
@@ -0,0 +1,95 @@
+// pages/component/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array: ['思存工作室', '外联部', '监察部', '媒体组'],
+    index: 0,
+    date: '2016-09-01',
+    time: '12:01',
+    region:['湖北', '武汉','武昌'],
+    customItem:''
+
+  },
+  bindPickerChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+
+  bindDateChange(e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindRegionChange(e) {
+    this.setData({
+      region: e.detail.value
+    })
+  },
+ 
+  bindTimeChange(e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
